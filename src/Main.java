9fa8b33ce58c34ecd481c4315fb41102d9408baf
shivamagrawal <shivam.agrawal@bajajfinserv.in>
2023-04-12 08:39:44 +0530

    String Array comparator custom sorting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String[] array = {"Are","we","Really","The","World"};
+        Arrays.sort(array, new StringCompare());      //Custom sorting
+        System.out.println(Arrays.toString(array));
     }
 }
